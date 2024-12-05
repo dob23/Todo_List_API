f1e56f505ed053766558e216ec7d8e7ebb3bce76
Daniel <danbedoya2001@gmail.com>
2024-12-05 11:35:33 -0500

    vista del login y configuracion de la url

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from user.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(),name="login")
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import authenticate
 # Create your views here.
+class LoginView(APIView):
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            raise AuthenticationFailed('Debe Proporcionar un usuario y una contraseña')
+        
+        user = authenticate(request, username, password = password)
+        
+        if user is None:
+            raise AuthenticationFailed('Credenciales Incorrectas')
+        
+        return Response({
+            'message': 'Autenticacion exitosa',
+            'user': {
+                'username': user.username,
+                'email': user.email,
+            }
+        })
